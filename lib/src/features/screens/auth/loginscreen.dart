914d16f2bd1d56b50b2de94a9b40697842e25af7
Diaa Almasri <diaaalmasri825@gmail.com>
2024-12-03 08:24:41 +0000

    Update loginscreen.dart

--- lib/src/features/screens/auth/loginscreen.dart
+++ lib/src/features/screens/auth/loginscreen.dart
@@ -1,9 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:path/path.dart';
 import 'package:program_language_project/src/core/components/buttons/App_button.dart';
 import 'package:program_language_project/src/core/components/inters/app_input.dart';
+import 'package:program_language_project/src/core/components/widgets/bounded_list_view.dart';
 import 'package:program_language_project/src/core/constants/res.dart';
 
 class Loginscreen extends StatelessWidget {
@@ -12,39 +12,40 @@ class Loginscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BoundedListView(
+      body: Column(
         children: [
           Expanded(child: SvgPicture.asset(Res.login)),
           Expanded(
-              child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 36),
-                child: AppInput(
-                  isEnabled: true,
-                  hint: 'Phone number',
-                ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36),
+              child: BoundedListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: AppInput(
+                      isEnabled: true,
+                      hint: 'Phone number',
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: AppInput(
+                      isEnabled: false,
+                      hint: 'Password',
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: AppButton(isLoading: false, text: 'Login'),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('I have already account'),
+                  ),
+                ],
               ),
-              SizedBox(height: 16),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 36),
-                child: AppInput(
-                  isEnabled: false,
-                  hint: 'password',
-                ),
-              ),
-              SizedBox(height: 106),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 65),
-                child: AppButton(isLoading: false, text: 'Login'),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              TextButton(
-                  onPressed: () {}, child: Text('I have already account')),
-            ],
-          ))
+            ),
+          ),
         ],
       ),
     );
